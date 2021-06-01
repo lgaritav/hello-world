05dd589a5054dd69d6dcc434a4dbee2694646b08
releasemanager <releasemanagergbm@bitbucket.org>
2021-06-01 16:14:56 -0600

    prueba

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express') 
 var app = express()
 	app.get('/', function (req, res) {   
-		res.send('Hello World!') 
-	})   
-	app.listen(3000, function () {   
-		console.log('Example app listening on port 3000!') })  
-	module.exports = app;
+		res.send('Hello World!');
+		app.close();
+		process.exit(0);
+	});
+	app.get('/quit', function (req, res) {   
+	  res.send('close');
+	});	
+	app.listen(3000);
+module.exports = app;
