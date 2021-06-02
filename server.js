ef798b51c6e3d4cbe6946eb088b0ca1b24994b94
releasemanager <releasemanagergbm@bitbucket.org>
2021-06-02 16:45:02 -0600

    prueba de error

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express') 
 var app = express()
 	app.get('/', function (req, res) {   
-		res.send('Hello World!');
-		return 'Hello World!'
+		res.send('Hola.... World!');
+		return 'Hello World!';
 	});
 	app.get('/quit', function (req, res) {   
 	  res.send('close');
