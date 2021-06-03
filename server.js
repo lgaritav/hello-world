d2782b16967af6dadf0a9f1630ef36813bee8c57
Laura Cristina Garita <lgarita@gbm.net>
2021-06-03 20:55:10 +0000

    Updated server.js, con prueba correcta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express') 
 var app = express()
 	app.get('/', function (req, res) {   
-		res.send('Hola...... World!');
+		res.send('Hello World!');
 		return 'Hello World!';
 	});
 	app.get('/quit', function (req, res) {   
